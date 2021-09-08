219271fcdd2dbbdd86cab17b7642572bfc2ded2f
Aaqil Shihab <aaqil@aaqil.com>
2021-09-09 01:27:42 +0530

    Added docstring to views

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Blog-API
+
+Simple API created with Django Rest Framework to a Blog application.
+
--- api/views.py
+++ api/views.py
@@ -22,6 +22,7 @@ def APIOverview(request):
 
 
 class UserListView(generics.ListAPIView):
+    """List all users, Only admin can access"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (permissions.IsAdminUser,)
@@ -30,6 +31,7 @@ class UserListView(generics.ListAPIView):
 
 
 class UserPasswordChangeView(generics.UpdateAPIView):
+    """View for changing the password of auhtorized user"""
     serializer_class = UserPasswordChangeSerializer
     queryset = User.objects.all()
     permission_classes = (IsUserOrReadOnly,)
@@ -45,39 +47,41 @@ class UserPasswordChangeView(generics.UpdateAPIView):
         return Response(data={'password changed': 'Successful'}, status=status.HTTP_200_OK)
 
 
-@api_view(['PATCH', 'PUT'])
-@permission_classes([IsUserOrReadOnly])
-def UserProfileEdit(request, *args, **kwargs):
-    print(request.user)
-    user = User.objects.get(id=request.user.id)
-    if user:
-        serializer = UserSerializer(
-            user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data='Success')
-    return Response('Not success')
+# @api_view(['PATCH', 'PUT'])
+# @permission_classes([IsUserOrReadOnly])
+# def UserProfileEdit(request, *args, **kwargs):
+#     print(request.user)
+#     user = User.objects.get(id=request.user.id)
+#     if user:
+#         serializer = UserSerializer(
+#             user, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data='Success')
+#     return Response('Not success')
 
 
 class UserCreateview(generics.CreateAPIView):
+    """View for registering new user"""
     queryset = User.objects.all()
     serializer_class = UserRegisterSerializer
     permission_classes = (permissions.AllowAny,)
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        print(response.data)
         token, _ = Token.objects.get_or_create(user=response.data['id'])
         return Response(({token.key}))
 
 
 class UserUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    """View for get,update, patch and delete user instance"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsUserOrReadOnly,)
 
 
 class PostListView(generics.ListCreateAPIView):
+    """View for get all the post and create new post"""
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
 
@@ -96,12 +100,14 @@ class PostListView(generics.ListCreateAPIView):
 
 
 class PostUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    """Get,update,patch or delete post instance. Only avalible to post author"""
     queryset = Post.objects.all()
     serializer_class = PostCreateSerializer
     permission_classes = (IsAuthorOrReadOnly,)
 
 
 class CommentListView(generics.ListCreateAPIView):
+    """Create and get comments"""
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
@@ -121,12 +127,14 @@ class CommentListView(generics.ListCreateAPIView):
 
 
 class CommentUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    """get,update,patch and delete comment instance, Only avaliable to comment author"""
     queryset = Comment.objects.all()
     serializer_class = CommentCreateSerializer
     permission_classes = (IsAuthorOrReadOnly,)
 
 
 class CommentPostView(generics.ListAPIView):
+    """Get comments that belongs to user"""
     serializer_class = CommentPostSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
